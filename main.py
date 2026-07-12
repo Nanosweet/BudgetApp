12f019a94e18a48af7d9341d6cf6cf303f12a3ea
Nanosweet <34351656+Nanosweet@users.noreply.github.com>
2026-07-12 21:14:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,3 @@ sort_naziwsko = sorted(data, key=lambda x: x['nazwisko'])
 pracownicy_z_klasy = EmployeeService(data)
 
 report.print_result(pracownicy_z_klasy.filter_department('HR'))
-#for p in pracownicy_z_klasy.filter_department('IT')
-
-#for p in sort_naziwsko:
-#    if p['dzial'] == 'IT':
-#        report.print_result(p)
