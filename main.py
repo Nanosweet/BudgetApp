951a80cb8ff68e458a54657e9e7edbc367523880
Nanosweet <34351656+Nanosweet@users.noreply.github.com>
2026-07-14 12:58:27 +0200

    update

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from repositories.csv_repository import CsvRepository
 from report import Report
 from services.employee_service import EmployeeService
 from services.products_service import ProductsService
-
+from services.grade_service import GradeService
 
 repo = CsvRepository()
 report = Report()
@@ -11,7 +11,7 @@ data_employees = repo.load(files['pracownicy'])
 sort_naziwsko = sorted(data_employees, key=lambda x: x['nazwisko'])
 pracownicy = EmployeeService(data_employees)
 produkty = ProductsService(repo.load(files['produkty']))
-
+oceny = GradeService(repo.load(files['oceny_uczniow']))
 
 def zadanie1_1():  
     report.print_result(pracownicy.get_employees())
@@ -24,16 +24,20 @@ def zadanie1_3():
 
 def zadanie2_1a():
     report.result(pracownicy.get_avarage_salary())
+    
 def zadanie2_1b():
     report.result(pracownicy.get_lowest_salary())
+    
 def zadanie2_1b2():
     report.result(pracownicy.get_highest_salary())
-def zadanie2_1c():
-    report.result(pracownicy.get_avg_salary_by_department('IT'))
-
     
+def zadanie2_1c():
+    report.result(pracownicy.get_avg_salary_by_department('HR'))
+    
+def zadanie2_2a():
+    report.print_result(oceny.get_avg_grade())
 
-
-
+def zadanie2_2c():
+    report.result(oceny.get_empty_grade())
 #
-zadanie2_1b()
+zadanie2_2c()
---
+++ services/grade_service.py
@@ -0,0 +1,37 @@
+class GradeService:
+    def __init__(self, students):
+        self.students = students
+        
+    @property
+    def przedmioty(self):
+        return [
+            'matematyka',
+            'fizyka',
+            'historia',
+            'jezyk_angielski'
+        ]
+        
+    def get_avg_grade(self):
+        srednie = []
+        for oceny in self.students:
+            suma = 0
+            licznik = 0
+            
+            for p in self.przedmioty:
+                if oceny[p] != "":
+                    ocena = int(oceny[p])
+                    suma += ocena
+                    licznik +=1
+                    
+            srednia = suma / licznik if licznik > 0 else 0
+            srednie.append((oceny['imie'], round(srednia, 1)))
+            
+        return srednie
+    def get_empty_grade(self):
+        empty = []
+        for s in self.students:
+            for p in self.przedmioty:
+                if s[p] == "":
+                    empty.append(s['imie'])
+        return f"Oceny brakuje co najmniej {len(empty)} uczniom"
+    
